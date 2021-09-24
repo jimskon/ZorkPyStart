a59b5656bfff2b148eb7f246d3b9553b905699a2
Jim Skon <jamesskon@gmail.com>
2021-09-24 11:34:14 -0400

    Remove jquery

--- zork.js
+++ zork.js
@@ -2,39 +2,38 @@
 var id="jimskon"
 
 var world;
-// Actions to get things started
-$(document).ready(function () {
 
-    $('#command-btn').click(command);
+// Add a click event for the command button
+document.querySelector("#command-btn").addEventListener("click", (e) => {
+    command();
+});
 
-    var input = document.getElementById("command");
-    // Respond to enter key
-    input.addEventListener("keyup", function(event) {
-      // Number 13 is the "Enter" key on the keyboard
-      if (event.keyCode === 13) {
+var input = document.getElementById("command");
+// Respond to enter key
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
         // Cancel the default action, if needed
         event.preventDefault();
         // Trigger the button element with a click
         command();
-      }
-    });
-
-    processCommand("Start");
+    }
 });
 
+processCommand("Start");
 
 
 // Print to terminal, and clear the input
 function printLine(text) {
-  $('#term').append(text+"<br />");
-  var objDiv=$('#term')[0];
-  objDiv.scrollTop = objDiv.scrollHeight;
+    term = document.getElementById("term")
+    term.innerHTML = term.innerHTML+text+"<br />";
+    term.scrollTop = term.scrollHeight;
 }
 
 //Input and process Command
 // Called from submit button or enter
 function command() {
-  com=$('#command').val();
+  com=document.getElementById("command").value;
   printLine("<span style='color:blue'>>>> "+com+"</span>");
   processCommand(com.toLowerCase());
 }
@@ -45,18 +44,16 @@ function processCommand(command) {
     }
     command = command.split(' ').join('+');
     console.log(command,id);
-    $.ajax(
-    {
-    type: "get",
-    url: "/cgi-bin/skon_gamewebclient.py?id=" + id + "&command=" + command,
-    dataType: "text",
-    success:  processResults,
-    error: function(request, ajaxOptions, thrownError)
-    {
-        $("#debug").text("error with get:"+request+thrownError);
-    }
-  });
+    fetch('/cgi-bin/skon_gamewebclient.py?id=' + id + '&command=' + command, {
+	method: 'get'
+    })
+	.then (response => response.text() )
+        .then (data => processResults(data))
+	.catch(error => {
+	    {alert("Error: Something went wrong:"+error);}
+	})
 }
+
 
 function processResults(results) {
   console.log(results);
